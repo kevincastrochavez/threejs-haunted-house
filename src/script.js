3ab6932647b52f20557f9106beb4523a4c35b340
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-29 20:21:14 -0700

    added fog and blended scene into same color for the borders

--- src/script.js
+++ src/script.js
@@ -15,6 +15,10 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
+// Fog
+const fog = new THREE.Fog('#262837', 1, 15);
+scene.fog = fog;
+
 /**
  * Textures
  */
@@ -177,6 +181,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor('#262837');
 
 /**
  * Animate
